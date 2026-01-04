94aff412062dba63f48e337d736f5f20b5e1620a
Saurav <sukumarsaurav@gmail.com>
2026-01-04 20:49:38 +0530

    Fix duplicate index warning on campaigns.slug

--- database/migrations/013_campaign_manager.sql
+++ database/migrations/013_campaign_manager.sql
@@ -8,7 +8,7 @@
 CREATE TABLE IF NOT EXISTS `campaigns` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL COMMENT 'Campaign display name',
-    `slug` VARCHAR(100) NOT NULL UNIQUE COMMENT 'URL-safe identifier',
+    `slug` VARCHAR(100) NOT NULL COMMENT 'URL-safe identifier',
     
     -- UTM Parameters
     `utm_source` VARCHAR(100) NOT NULL COMMENT 'Traffic source: google, facebook, linkedin, etc.',
